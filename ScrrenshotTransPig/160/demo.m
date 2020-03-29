707691b67442813ac8179ea766685fca22db4ff3
imangoa <3508312371@qq.com>
2020-03-30 04:42:16 +0800

    增加 demo.m

--- ScrrenshotTransPig/160/demo.m
+++ ScrrenshotTransPig/160/demo.m
@@ -3,5 +3,155 @@ image2gray=rgb2gray(image);
 T = graythresh(image2gray);
 M = imbinarize(image2gray,T);%二值化
 
+% 变量申明
+M_left = [] ;  %记录左边线的轨迹
+M_right = [] ; %记录右边线的轨迹
+flag = 0;     %判断图的三种类型。
+
+
+
+
+% 最左边为白色,最右边为黑色时
+
+if M(63,0)==1&&M(63,120)==0
+    flag = 0;           % 将flag置为0
+    num =0;             %计数
+    i = 63;
+
+    while M(i,0)==1      %得到最左边白点连续扩展到的最高点          
+        M_left(num*2)=i;  %记录纵坐标    
+        M_left(um*2+1)=0;  %记录横坐标
+        i=i-1;        
+        num=num+1;
+    end
+
+    i=120;
+    while 1           %得到左边为白点的黑点
+        if M(63,i)==0&&M(63,i-1)&&M(63,i-2)==1&&M(63,i-3)==1
+            M_right(0) =63;
+            M_right[(1) = i;
+            break;
+        end
+        i=i-1;
+        
+    end
+    
+    
+
+
+
+% 最左边为黑色,最右边为白色时
+%代码片段与flag==0类似
+
+elseif M(63,120)==1&&M(63,0)==0
+    flag =1;       
+    num = 0;
+    i =63;
+
+    while M(i,120)
+        M_right(num*2) = i;
+        M_right(num*2+1) = 120;
+        i = i-1;
+        num = num+1;
+    end
+    
+    i = 0;
+    while 1
+        if M(63,i)==0&&M(63,i+1)==1&&M(63,i+2)==1&&M(63,i+3)==1
+            M_left(0)=63;
+            M_left(1) = i;
+            break;
+    end
+    i=i+1;
+
+
+
+    
+    %最左和最右边都为白线时,记录下来它们将划线的初始位置
+elseif M(63,0)==0&&M(63,120)==0
+    flag =2;
+    i = 0;
+    while 1
+        if M(63,i)==0&&M(63,i-1)&&M(63,i-2)==1&&M(63,i-3)==1
+            M_right(0)=63;
+            M_right(1) = i;
+            break;
+        end
+        i=i-1;        
+    end
+
+    i = 0;
+    while 1
+        if M(63,i)==0&&M(63,i+1)==1&&M(63,i+2)==1&&M(63,i+3)==1
+            M_left(0) =63;
+            M_left(1) = i;
+            break;
+    end
+    i=i+1;
+
+
+end
+
+
+
+
+
+%当处于第一种情况时，划线程序
+if flag==0
+    num = 0;
+    yi = M_left(0);
+    xi = M_left(1);
+    while M_left(2*num+1)<M_right(end)
+        num++;
+        if M(xi,yi+1)==0&&direction~=5
+            yi=yi+1;
+            coord_left(2*num+1)=xi;coord_left(2*num+2)=yi;
+            direction=1;
+            continue;
+        end
+        if M(xi-1,yi+1)==0
+            xi=xi-1;yi=yi+1;
+            coord_left(2*num+1)=xi;coord_left(2*num+2)=yi;
+            direction=2;
+            continue;
+        end
+        if M(xi-1,yi)==0
+            xi=xi-1;
+            coord_left(2*num+1)=xi;coord_left(2*num+2)=yi;
+            direction=3;
+            continue;
+        end
+        if M(xi-1,yi-1)==0
+            xi=xi-1;yi=yi-1;
+            coord_left(2*num+1)=xi;coord_left(2*num+2)=yi;
+            direction=4;
+            continue;
+        end
+        if M(xi,yi-1)==0
+           yi=yi-1;
+           coord_left(2*num+1)=xi;coord_left(2*num+2)=yi;
+           direction=5;
+           continue;
+        end
+        if M(xi+1,yi-1)==0
+           yi=yi-1;
+           coord_left(2*num+1)=xi;coord_left(2*num+2)=yi;
+           direction=6;
+           continue;
+        end
+        if M(xi+1,yi)==0
+           xi=xi+1;
+           coord_left(2*num+1)=xi;coord_left(2*num+2)=yi;
+           direction=7;
+           continue;
+        end   
+    end
+
+    
+
+
+
+
+
 
         
--- ScrrenshotTransPig/test.c
+++ ScrrenshotTransPig/test.c
@@ -2,33 +2,15 @@
 #define row 60
 #define height 120
 
-int look_point();      //寻找结点
-void draw_line();
+int test();
 
 int M[height][row];
 int M_right[height*row],M_left[height*row];
 
-int main()
-{
-    int flag;
-    flag =look_point();
-    draw_line(flag);  
-}
-
-
-
-void draw_line()
-{
-    
-}
-
-
-
-int look_point()
+int look_point()     //寻找结点
 {
 int flag;
-
- if (M[59][0]==1&&M[59][119]==0)
+    if (M[59][0]==1&&M[59][119]==0)
     {
         flag =0;
         int i= 59;
@@ -50,7 +32,6 @@ int flag;
         return flag;
             
     }
-
     
     else if (M[59][119]==1&&M[59][0]==0)
     {
@@ -103,3 +84,183 @@ int flag;
     }
        
 }
+void draw_line();
+
+
+
+int main()
+{
+    int flag;
+    flag =look_point();
+    draw_line(flag);  
+}
+
+
+
+void draw_line(flag)
+{
+    switch (flag)
+   {
+   case 0:
+        int a=1;
+        int xi=M_right[0];
+        int yi=M_left[1];
+        while (M_left[1]>=M_right[2*a+1])
+        {
+            a++;
+            if (M[xi,yi+1]==0)
+            {
+                yi++;
+                M_right[2*a]=xi;
+                M_right[2*a+1]=yi;
+                continue;
+            }
+            if (M[xi-1,yi+1]==0)
+            {
+                xi--;yi++;
+                 M_right[2*a]=xi;
+                M_right[2*a+1]=yi;
+                continue;
+            }
+            if (M[xi-1,yi]==0)
+            {
+                xi--;
+                M_right[2*a]=xi;
+                M_right[2*a+1]=yi;
+                continue;
+            }
+            if (M[xi,yi-1]==0)
+            {
+                yi--;/* code */
+                M_right[2*a]=xi;
+                M_right[2*a+1]=yi;
+                
+                continue;
+            }
+            if (M[xi+1,yi-1]==0)
+            {
+                yi--;/* code */
+                M_right[2*a]=xi;
+                M_right[2*a+1]=yi;
+                continue;
+            }
+            if (M[xi+1,yi]==0)
+            {
+                xi++;
+                M_right[2*a]=xi;
+                M_right[2*a+1]=yi;
+                continue;
+            }            
+        }
+        
+       break;
+   case 1:
+       int a=1;
+
+        int xi=M_left[0];
+        int yi=M_left[1];
+        while (M_right[1]>=M_left[2*a+1])
+        {
+            a++;
+            if (M[xi,yi+1]==0)
+            {
+                yi++;
+                M_right[2*a]=xi;
+                M_right[2*a+1]=yi;
+                continue;
+            }
+            if (M[xi-1,yi+1]==0)
+            {
+                xi--;yi++;
+                 M_right[2*a]=xi;
+                M_right[2*a+1]=yi;
+                continue;
+            }
+            if (M[xi-1,yi]==0)
+            {
+                xi--;
+                M_right[2*a]=xi;
+                M_right[2*a+1]=yi;
+                continue;
+            }
+            if (M[xi,yi-1]==0)
+            {
+                yi--;/* code */
+                M_right[2*a]=xi;
+                M_right[2*a+1]=yi;
+                continue;
+            }
+            if (M[xi+1,yi-1]==0)
+            {
+                yi--;/* code */
+                M_right[2*a]=xi;
+                M_right[2*a+1]=yi;
+                continue;
+            }
+            if (M[xi+1,yi]==0)
+            {
+                xi++;
+                M_right[2*a]=xi;
+                M_right[2*a+1]=yi;
+                continue;
+            }            
+        }
+        
+       break;
+   case 2:
+        int R_xi=M_right[0];
+        int R_yi=M_right[1];
+        int L_xi=M_left[0];
+        int L_yi=M_left[1];
+        while (1)
+        {
+            while (1)
+            {
+                a++;
+            if (M[xi,yi+1]==0)
+            {
+                yi++;
+                M_right[2*a]=xi;
+                M_right[2*a+1]=yi;
+                break;
+            }
+            if (M[xi-1,yi+1]==0)
+            {
+                xi--;yi++;
+                 M_right[2*a]=xi;
+                M_right[2*a+1]=yi;
+                break;
+            }
+            if (M[xi-1,yi]==0)
+            {
+                xi--;
+                M_right[2*a]=xi;
+                M_right[2*a+1]=yi;
+                continue;
+            }
+            if (M[xi+1,yi]==0)
+            {
+                xi++;
+                M_right[2*a]=xi;
+                M_right[2*a+1]=yi;
+                continue;
+            }            
+        }
+
+            }
+            while (1)
+            {
+                test();
+                break;           //如果，
+            }
+
+        }
+        
+       break;
+   case 3:
+   default:
+       break;
+}
+
+
+
